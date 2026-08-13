7bd7cd3c7acba256dc5d2dfe2ae83b2b743f5b6e
Ankit Kumar <ankitkumarswe01@gmail.com>
2026-08-13 21:04:28 +0530

    upd

--- insert.mongodb.js
+++ insert.mongodb.js
@@ -75,6 +75,7 @@ db.students.find();
 db.students.find({city:{$in :["Delhi", "Bhopal"]}})
 db.students.find({city:{$in :["Ahemdabaad", "jaipur"]}})
 db.students.find({age:{$eq : 22}})
+db.students.find({age:{$eq : 22}})
 
 
 
